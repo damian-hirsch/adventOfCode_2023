389a06a8db7bc80f9edf9da57b45ba2e5880a8a0
damian-hirsch <damianghirsch@gmail.com>
2023-11-29 18:21:22 +0100

    Initial commit

---
+++ Day06.py
@@ -0,0 +1,26 @@
+# Get data from .txt file
+def get_input() -> list:
+    with open('input/Day06.txt', 'r') as file:
+        # Remove space, split lines, and write each line to list
+        data = file.read().replace(' ', '').splitlines()
+    return data
+
+
+# Solves part 1
+def part_one(data: list) -> int:
+    return 1
+
+
+# Solves part 2
+def part_two(data: list) -> int:
+    return 1
+
+
+def main():
+
+    print('The score would be:', part_one(get_input()))
+    print('The score would be:', part_two(get_input()))
+
+
+if __name__ == '__main__':
+    main()
