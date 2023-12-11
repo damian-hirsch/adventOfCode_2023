b1792040f15bf23e58b5643a40f8d326ac810f7c
damian-hirsch <damianghirsch@gmail.com>
2023-12-11 09:45:10 +0100

    Day 11

--- Day11.py
+++ Day11.py
@@ -1,25 +1,53 @@
+import numpy as np
+
+
 # Get data from .txt file
-def get_input() -> list:
+def get_input() -> np.ndarray:
     with open('input/Day11.txt', 'r') as file:
-        # Remove space, split lines, and write each line to list
-        data = file.read().replace(' ', '').splitlines()
-    return data
+        # Split lines
+        data = file.read().splitlines()
+        observed = np.full((len(data), len(data[0])), '', dtype='U1')
+        for i, line in enumerate(data):
+            observed[i, :] = list(line)
+    return observed
 
 
-# Solves part 1
-def part_one(data: list) -> int:
-    return 1
+# Solves part 1 and part 2
+def part_one_and_two(data: np.ndarray, scale: int) -> int:
+    # Check if all elements in each row are equal to '.'
+    all_dots_per_row = np.all(data == '.', axis=1)
+    # Check if all elements in each column are equal to '.'
+    all_dots_per_column = np.all(data == '.', axis=0)
 
+    # List to store the positions of '#'
+    positions = []
 
-# Solves part 2
-def part_two(data: list) -> int:
-    return 1
+    # Iterate through each element in the array
+    for j in range(data.shape[0]):  # Loop over rows
+        # Correction value depending on how many empty rows we have
+        y_correction = (scale - 1) * sum(all_dots_per_row[:j])
+        for i in range(data.shape[1]):  # Loop over columns
+            # Correction value depending on how many empty columns we have
+            x_correction = (scale - 1) * sum(all_dots_per_column[:i])
+            # Check if we have a galaxy
+            if data[j, i] == '#':
+                # Add position with corrected for the spacing
+                positions.append((j + y_correction, i + x_correction))
+
+    # Check all combinations and calculate distances
+    distances = []
+    for position_1 in positions:
+        for position_2 in positions:
+            distances.append(abs(position_1[0] - position_2[0]) + abs(position_1[1] - position_2[1]))
+
+    # Because we also have the reverse combination, we need to divide by 2
+    return sum(distances) // 2
 
 
 def main():
 
-    print('The score would be:', part_one(get_input()))
-    print('The score would be:', part_two(get_input()))
+    print('The sum of these lengths is:', part_one_and_two(get_input(), 2))
+    print('The sum of these lengths is:', part_one_and_two(get_input(), 1000000))
 
 
 if __name__ == '__main__':
