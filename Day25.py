b787c919e0d002ba57701fa6082d9178485fcbe8
damian-hirsch <damianghirsch@gmail.com>
2023-12-25 07:31:28 +0100

    Day 25

--- Day25.py
+++ Day25.py
@@ -1,25 +1,42 @@
+import networkx as nx
+
+
 # Get data from .txt file
-def get_input() -> list:
+def get_input() -> nx.Graph:
     with open('input/Day25.txt', 'r') as file:
-        # Remove space, split lines, and write each line to list
-        data = file.read().replace(' ', '').splitlines()
-    return data
+        # Split lines
+        data = file.read().splitlines()
+        # Initialize graph
+        graph = nx.Graph()
+        for line in data:
+            connector, cns = line.split(': ')
+            for cn in cns.split():
+                # Add edges to graph
+                graph.add_edge(connector, cn)
+    return graph
 
 
 # Solves part 1
-def part_one(data: list) -> int:
-    return 1
+def part_one(graph: nx.Graph) -> int:
+    # Minimum edge cut finds the smallest set of edges that you can remove to disconnect the graph, which will be our 3
+    # edges. We then remove these edges from the graph.
+    graph.remove_edges_from(nx.minimum_edge_cut(graph))
 
+    # Initialize result
+    result = 1
+    # connected_components returns the set of components belonging to each subgraph (there should be two now, because we
+    # disconnected it before)
+    for components in nx.connected_components(graph):
+        # Count the length and multiply
+        result *= len(components)
 
-# Solves part 2
-def part_two(data: list) -> int:
-    return 1
+    return result
 
 
 def main():
 
-    print('The score would be:', part_one(get_input()))
-    print('The score would be:', part_two(get_input()))
+    print('If you multiply the sizes of these two groups together you get:', part_one(get_input()))
+    print('The last star you get automatically for gathering all others')
 
 
 if __name__ == '__main__':
